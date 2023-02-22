510957717890a4a78fb4c259b9364f879f20fb35
jvanin <jvanin@iu.edu>
2023-02-22 07:19:21 -0500

    Changes in the number of parameters as example

--- Models/vCornea_4_8/Simulation/BatchTestInputs.py
+++ Models/vCornea_4_8/Simulation/BatchTestInputs.py
@@ -4,8 +4,17 @@ __param_desc__ = {}
 
 # List of Parameters to change
 
-__param_desc__["PARAMETER_NAME"] = "PARAMETER DESCRIPTION"
+__param_desc__["PARAMETER_NAME"] = "(optional) PARAMETER DESCRIPTION"
 
-PARAMETER_NAME = 1 # PARAMETER VALUE
+# PARAMETER VALUE
+PARAMETER_NAME = 1  # Best to use one since the batch execution uses the multiplier function 
+                    # meaning the number you define in batch_exec.py file will be the actual 
+                    # tested parameter
 
+__param_desc__["PARAMETER_NAME2"] = "(optional) PARAMETER2 DESCRIPTION"
 
+PARAMETER_NAME2 = 1
+
+__param_desc__["PARAMETER_NAME3"] = "(optional) PARAMETER3 DESCRIPTION"
+
+PARAMETER_NAME3 = 1
